7a9e9c9e37cb73302138e0d8e8c4fe9faebdbee4
ahmedtechm <ahmedtechm2050@gmail.com>
2023-06-18 11:11:35 +0400

    Added Linux Api Application Project to GitHub

---
+++ src/test/java/com/Linux/linuxapi/LinuxApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Linux.linuxapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinuxApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
