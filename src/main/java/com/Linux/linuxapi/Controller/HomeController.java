2529222fa9b10bf83a104c7e9b056918fb636f21
ahmedtechm <ahmedtechm2050@gmail.com>
2023-06-18 11:38:15 +0400

    Added GET mapping for the home page message in HomeController class

--- src/main/java/com/Linux/linuxapi/Controller/HomeController.java
+++ src/main/java/com/Linux/linuxapi/Controller/HomeController.java
@@ -1,8 +1,15 @@
 package com.Linux.linuxapi.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(path = "/")
 public class HomeController {
 
+    @GetMapping
+    public String getHomePageMessage() {
+        return "Hello Batch Two, Nice to see you";
+    }
 }
